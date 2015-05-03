d4567348c8e72213748ba3606e5208de7c0fd2cf
Eroche5 <Eroche5@gmail.com>
2015-05-03 14:20:14 -0600

    Figuring out how to filter the data by work group

--- LoadDataScript.R
+++ LoadDataScript.R
@@ -1,6 +1,32 @@
 require(RSocrata)
+require(dplyr)
 
+
+#Load 311 Data
 token <- "JJ_K1U6j1aV1y-3L4BQEDk6gPmdsqYf_evZg"
 Data.311 <- read.socrata("https://data.kcmo.org/311/KCMOPS311-Data/7at3-sxhp")
-nrow(Data.311)
 
+  #Inspect the data
+  nrow(Data.311)
+  Work.Groups <- names(table(Data.311$WORK.GROUP))
+  
+  #Clean Data
+  Data.311$CREATION.DATE <- as.POSIXct(Data.311$CREATION.DATE)
+  Data.311$CLOSED.DATE <- as.POSIXct(Data.311$CLOSED.DATE)
+  
+###
+# 311 Volume by Month for a defined timeframe
+###
+
+#Grouping Data
+  Work.Group.DF <- dplyr::filter(Data.311, WORK.GROUP %in% "Public Works-Street and Traffic-Streetlights")
+  
+
+require(ggplot2)
+p <- ggplot(Work.Group.DF, aes(x=CREATION.YEAR, y=count(CASE.ID), group=CREATION.MONTH))
+p + geom_line()
+
+
+
+
+  
