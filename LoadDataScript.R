09d45ee560762989a1cad3d4afbc1b305702b508
Eroche5 <Eroche5@gmail.com>
2015-05-07 18:52:15 -0600

    Fixed the POSIXct issue. Implemented a crude graph. Added SD upper/lower bounds. Inserted a needs reminder list.

--- LoadDataScript.R
+++ LoadDataScript.R
@@ -1,38 +1,61 @@
 require(RSocrata)
 require(plyr)
 require(dplyr)
+require(lubridate)
+require(ggplot2)
 
 
 #Load 311 Data
-token <- "JJ_K1U6j1aV1y-3L4BQEDk6gPmdsqYf_evZg"
+token <- JJ_K1U6j1aV1y-3L4BQEDk6gPmdsqYf_evZg
 Data.311 <- read.socrata("https://data.kcmo.org/311/KCMOPS311-Data/7at3-sxhp")
 
-  #Inspect the data
-  nrow(Data.311)
-  Work.Groups <- names(table(Data.311$WORK.GROUP))
-  
-  #Clean Data
-  Data.311$CREATION.DATE <- as.POSIXct(Data.311$CREATION.DATE)
-  Data.311$CLOSED.DATE <- as.POSIXct(Data.311$CLOSED.DATE)
-  
+#Inspect the data
+nrow(Data.311)
+Work.Groups <- names(table(Data.311$WORK.GROUP))
+
+#Clean Data
+Data.311$CREATION.DATE <- as.POSIXct(Data.311$CREATION.DATE)
+Data.311$CLOSED.DATE <- as.POSIXct(Data.311$CLOSED.DATE)
+
+
 ###
 # 311 Volume by Month for a defined timeframe
 ###
 
-  #Filter to Work Group you are interested in
-  Work.Group.DF <- dplyr::filter(Data.311, WORK.GROUP %in% "Public Works-Street and Traffic-Streetlights")
-  
-  #Add a column in the "M-Y" format. For rolling up I guess?
-  Work.Group.DF$Creation.Month.Year <- strftime(Work.Group.DF$CREATION.DATE, format="%m/%Y")
+#Filter to Work Group you are interested in
+Work.Group.DF <- dplyr::filter(Data.311, WORK.GROUP %in% "Public Works-Street and Traffic-Streetlights")
 
-  Output <- ddply(Work.Group.DF, c("Creation.Month.Year"), summarise,
-                 Numbe.Of.Cases    = length(CASE.ID),
-                 Mean.Days.To.Close = mean(DAYS.TO.CLOSE, na.rm=TRUE),
-                 sd   = sd(DAYS.TO.CLOSE, na.rm=TRUE)
-                )
-                 
+#Add a column in the "M-Y" format. For rolling up I guess?
+Work.Group.DF$Creation.Month.Year <- strftime(Work.Group.DF$CREATION.DATE, format="%m%Y")
+
+Output <- ddply(Work.Group.DF, c("Creation.Month.Year"), summarise,
+                Number.Of.Cases    = length(CASE.ID),
+                Mean.Days.To.Close = mean(DAYS.TO.CLOSE, na.rm=TRUE),
+                Standard.Deviation  = sd(DAYS.TO.CLOSE, na.rm=TRUE), 
+                Median.Days.To.Close = median(DAYS.TO.CLOSE, na.rm=TRUE)
+)
+
+
 ##How do I rollup by dates? Test <- is trying to format my new column as posix but it isn't working right. 
 #Can I just use the original full creation date and just rollup by months?
+Output$Creation.Month.Year <-  parse_date_time(Output$Creation.Month.Year, order="my")
+Output[order(Output$Creation.Month.Year , decreasing = FALSE ),]
 
+#add in top and bottom sd
+require(dplyr)
+Output <- mutate(Output, "Upper SD Limit"=  Mean.Days.To.Close + Standard.Deviation) 
+Output <- mutate(Output, "Lower SD Limit" = Mean.Days.To.Close - Standard.Deviation)
 
-  
+#plot
+p <- ggplot(Output, aes(x=Creation.Month.Year, y=Number.Of.Cases, group= 1))
+p + geom_line(size = 2)
+
+## To fix##
+#Need to fix labels for chart.
+#Need to smooth line on chart
+#Need to make labels more attractive
+#Need to add title to chart that pulls from the selected workgroup
+#need to generate a second chart with median days to close + SD's
+#Need to implement timeframe selection
+#Create timeframe vectors with Fiscal Years
+#Need to be able to have standard "from" and "to" fields 
