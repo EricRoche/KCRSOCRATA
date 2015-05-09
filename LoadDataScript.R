11d46188ba7e9104355c71f72cfd54980b21a180
Eroche5 <Eroche5@gmail.com>
2015-05-09 11:43:05 -0600

    This copy actually works. Still need to add a scaling technique.

--- LoadDataScript.R
+++ LoadDataScript.R
@@ -3,6 +3,7 @@ require(plyr)
 require(dplyr)
 require(lubridate)
 require(ggplot2)
+library(scales)
 
 
 #Load 311 Data
@@ -22,8 +23,15 @@ Data.311$CLOSED.DATE <- as.POSIXct(Data.311$CLOSED.DATE)
 # 311 Volume by Month for a defined timeframe
 ###
 
-#Filter to Work Group you are interested in
+###INPUTS###
 TargetWG <- "Public Works-Street and Traffic-Streetlights"
+By.Day <- "1 day"
+By.Month <- "1 month"
+By.Year <-  "1 year"
+From.Month.Year <-  as.POSIXct("2010-01-01 UTC")
+To.Date <- as.POSIXct("2012-01-01 UTC")
+
+#Filter to Work Group you are interested in
 Work.Group.DF <- dplyr::filter(Data.311, WORK.GROUP %in% ( TargetWG))
 
 #Add a column in the "M-Y" format. For rolling up I guess?
@@ -48,27 +56,46 @@ Output <- mutate(Output, "Upper SD Limit"=  Mean.Days.To.Close + Standard.Deviat
 Output <- mutate(Output, "Lower SD Limit" = Mean.Days.To.Close - Standard.Deviation)
 
 #plot
-p <- ggplot(Output, aes(x=Creation.Month.Year, y=Number.Of.Cases, group= 1))
+p <- ggplot(Output, aes(x=Creation.Month.Year, y=Number.Of.Cases, group= 1)) 
 p2 <- p + geom_line(size = 2) + 
           stat_smooth(
                       se = F,
                       size = 2, 
                       alpha = 1
                       )+
+        #Date - Picks this from an earlier value
+        scale_x_datetime(labels=date_format("%m-%Y"),
+                         breaks = date_breaks(By.Month))+
+        #Labels
           labs(title = TargetWG,
-              x = "Creation Month - Year"
-              y = "Total Number of Cases Per Month")
+              x = "Creation Month - Year",
+              y = "Total Number of 311 Cases Created Per Month") +
+          theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+        #Theme
+          theme(axis.text=element_text(size=10),
+                axis.title=element_text(size=18,face=))+
+          theme(plot.title = element_text(size=18, face="bold"))
+  
+print(p2)        
+
+
+  
+          
 
 
 
 ggsave(p2, file="sample.jpg", dpi = 600)
 
 ## To fix##
-#Need to fix labels for chart.
-#Need to smooth line on chart
-#Need to make labels more attractive
-#Need to add title to chart that pulls from the selected workgroup
+#Need to use a way to set "zoom paramaters" on chart
 #need to generate a second chart with median days to close + SD's
 #Need to implement timeframe selection
 #Create timeframe vectors with Fiscal Years
 #Need to be able to have standard "from" and "to" fields 
+
+##If you want to simply "zoom" in on a specific part of a plot, look at xlim() and ylim() respectively. 
+
+
+, 
+limits = (From.Month.Year), (To.Date)
+        )+
