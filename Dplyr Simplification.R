7c8d5434b510bc2ac1f4705b5c9814aeccbd9653
Eroche5 <Eroche5@gmail.com>
2015-08-16 20:08:44 -0500

    Fixed an as.numeric issue with exceeded estimated timeframe variable.

--- Dplyr Simplification.R
+++ Dplyr Simplification.R
@@ -1,7 +1,7 @@
 #Testing a giant output using dplyr
-require(RSocrata)
-require(dplyr)
-require(lubridate)
+library(RSocrata)
+library(dplyr)
+library(lubridate)
 library(scales)
 
 token <- XXXXXXX
@@ -16,6 +16,7 @@ Data.311$CREATION.DATE <- as.POSIXct(Data.311$CREATION.DATE)
 Data.311$CLOSED.DATE <- as.POSIXct(Data.311$CLOSED.DATE)
 Data.311$EXCEEDED.EST.TIMEFRAME[Data.311$EXCEEDED.EST.TIMEFRAME=="Y"] <- 1
 Data.311$EXCEEDED.EST.TIMEFRAME[Data.311$EXCEEDED.EST.TIMEFRAME=="N"] <- 0
+Data.311$EXCEEDED.EST.TIMEFRAME <- as.numeric(Data.311$EXCEEDED.EST.TIMEFRAME)
 Data.311 <- mutate(Data.311, Number.Of.Open.Cases = (STATUS))
 Data.311$Number.Of.Open.Cases[Data.311$Number.Of.Open.Cases == "OPEN"] <- 1
 Data.311$Number.Of.Open.Cases[Data.311$Number.Of.Open.Cases != "1"] <- 0
@@ -25,21 +26,17 @@ Data.311$Number.Of.Open.Cases <- as.numeric(Data.311$Number.Of.Open.Cases)
 
 group <- group_by(Data.311, DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH)
 stats <- summarise(group,
-           Number.Of.Cases = length(CASE.ID),
-           Number.Of.Open.Cases = sum(Number.Of.Open.Cases),
-           Number.Of.Cases.Exceeding.Timeframe = sum(EXCEEDED.EST.TIMEFRAME),
-           Percent.Of.Cases.Esceeding.Timeframe = (Number.Of.Cases.Exceeding.Timeframe/Number.Of.Cases)*100,
-           Mean.Days.To.Close = mean(DAYS.TO.CLOSE), 
-           Median.Days.To.Close = median(DAYS.TO.CLOSE),
-           Standard.Deviation = sd(DAYS.TO.CLOSE),
-           Above.One.Standard.Deviation = (Standard.Deviation + Mean.Days.To.Close),
-           Above.Two.Standard.Deviations = ((Standard.Deviation*2) + Mean.Days.To.Close),
-           Above.Three.Standard.Deviations = ((Standard.Deviation*3) + Mean.Days.To.Close),
-           Below.One.Standard.Deviation = (Mean.Days.To.Close - Standard.Deviation),
-           Below.Two.Standard.Deviations = (Mean.Days.To.Close - (Standard.Deviation*2)),
-           Below.Three.Standard.Deviations = (Mean.Days.To.Close - (Standard.Deviation*3))
-           )
-
-
-
-#Need to add a column that says the number of cases closed and one that says number remaining open
+                   Number.Of.Cases = length(CASE.ID),
+                   Number.Of.Open.Cases = sum(Number.Of.Open.Cases),
+                   Number.Of.Cases.Exceeding.Timeframe = sum(EXCEEDED.EST.TIMEFRAME),
+                   Percent.Of.Cases.Esceeding.Timeframe = (Number.Of.Cases.Exceeding.Timeframe/Number.Of.Cases)*100,
+                   Mean.Days.To.Close = mean(DAYS.TO.CLOSE), 
+                   Median.Days.To.Close = median(DAYS.TO.CLOSE),
+                   Standard.Deviation = sd(DAYS.TO.CLOSE),
+                   Above.One.Standard.Deviation = (Standard.Deviation + Mean.Days.To.Close),
+                   Above.Two.Standard.Deviations = ((Standard.Deviation*2) + Mean.Days.To.Close),
+                   Above.Three.Standard.Deviations = ((Standard.Deviation*3) + Mean.Days.To.Close),
+                   Below.One.Standard.Deviation = (Mean.Days.To.Close - Standard.Deviation),
+                   Below.Two.Standard.Deviations = (Mean.Days.To.Close - (Standard.Deviation*2)),
+                   Below.Three.Standard.Deviations = (Mean.Days.To.Close - (Standard.Deviation*3))
+)
