c1d6e160ce95e7923dab8b0728827fe54c5e3481
Eroche5 <Eroche5@users.noreply.github.com>
2015-12-13 14:32:14 -0600

    Update Pipes Test Script.R

--- Pipes Test Script.R
+++ Pipes Test Script.R
@@ -1,7 +1,7 @@
-#This script runs all of the summary statistics by group
+#P1 includes all data
 
 
-StatsPart1 <- Data.311 %>% 
+PipesTestP1 <- Data.311 %>% 
                 filter(STATUS == "RESOL" | STATUS == "OPEN") %>%
                   group_by(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH) %>%
                     summarise(
@@ -12,41 +12,35 @@ StatsPart1 <- Data.311 %>%
                       )
                       
 #P2 only calculated on closed data
-StatsPart2 <- Data.311 %>% 
+PipesTestP2 <- Data.311 %>% 
                 filter(STATUS == "RESOL" | STATUS == "OPEN" & length(CASE.ID) > 1) %>%
                   group_by(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH) %>%
                     summarise(
-                      Mean.Days.To.Close.Of.Closed.Cases = mean(DAYS.TO.CLOSE, na.rm=TRUE),
-                      Median.Days.To.Close.Closed.Cases = (median(DAYS.TO.CLOSE, na.rm=TRUE)*1),
-                      Mean.Days.Open.For.Open.And.Closed.Cases = mean(Days.Open),
-                      Median.Days.Open.For.Open.And.Closed.Cases = (median(Days.Open)*1),
-                      Standard.Deviation.of.Days.Open = sd(Days.Open, na.rm=TRUE),
-                      Above.One.Standard.Deviation = (Standard.Deviation.of.Days.Open + Mean.Days.Open.For.Open.And.Closed.Cases),
-                      Above.Two.Standard.Deviations = ((Standard.Deviation.of.Days.Open*2) + Mean.Days.Open.For.Open.And.Closed.Cases),
-                      Above.Three.Standard.Deviations = ((Standard.Deviation.of.Days.Open*3) + Mean.Days.Open.For.Open.And.Closed.Cases),
-                      Below.One.Standard.Deviation = (Mean.Days.Open.For.Open.And.Closed.Cases - Standard.Deviation.of.Days.Open),
-                      Below.Two.Standard.Deviations = (Mean.Days.Open.For.Open.And.Closed.Cases - (Standard.Deviation.of.Days.Open*2)),
-                      Below.Three.Standard.Deviations = (Mean.Days.Open.For.Open.And.Closed.Cases - (Standard.Deviation.of.Days.Open*3)))
-
-#Change all Standard deviations that are less than zero to zero so that they graph correctly. 
-#Negative SD's on days to close doesn't make sense and just messes up graphing.
-StatsPart2$Below.One.Standard.Deviation[StatsPart2$Below.One.Standard.Deviation <0] <- 0
-StatsPart2$Below.Two.Standard.Deviations[StatsPart2$Below.Two.Standard.Deviations <0] <- 0
-StatsPart2$Below.Three.Standard.Deviations[StatsPart2$Below.Three.Standard.Deviations <0] <- 0
+                      Mean.Days.To.Close.Of.Closed.Cases = mean(DAYS.TO.CLOSE),
+                      Median.Days.To.Close.Closed.Cases = (median(DAYS.TO.CLOSE)*1),
+                      Standard.Deviation = sd(DAYS.TO.CLOSE, na.rm=TRUE),
+                      Above.One.Standard.Deviation = (Standard.Deviation + Mean.Days.To.Close.Of.Closed.Cases),
+                      Above.Two.Standard.Deviations = ((Standard.Deviation*2) + Mean.Days.To.Close.Of.Closed.Cases),
+                      Above.Three.Standard.Deviations = ((Standard.Deviation*3) + Mean.Days.To.Close.Of.Closed.Cases),
+                      Below.One.Standard.Deviation = (Mean.Days.To.Close.Of.Closed.Cases - Standard.Deviation),
+                      Below.Two.Standard.Deviations = (Mean.Days.To.Close.Of.Closed.Cases - (Standard.Deviation*2)),
+                      Below.Three.Standard.Deviations = (Mean.Days.To.Close.Of.Closed.Cases - (Standard.Deviation*3)))
                 
 #Join the tables together
 #Add a unique identified that is shared between both datasets.
 
-StatsPart1 <- mutate(StatsPart1, UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
-StatsPart2 <- mutate(StatsPart2, UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
+PipesTestP1 <- mutate(PipesTestP1, UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
+PipesTestP2 <- mutate(PipesTestP2, UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
 
 #Drops duplicate columns but retains unique ID then combines the two data frames. 
-StatsPart2 <- subset(StatsPart2, select = -c(DEPARTMENT:CREATION.MONTH))
-StatsFull <- full_join(StatsPart1, StatsPart2, by = "UniqueID")
+PipesTestP2 <- subset(PipesTestP2, select = -c(DEPARTMENT:CREATION.MONTH))
+PipedTestAllData <- full_join(PipesTestP1, PipesTestP2, by = "UniqueID")
 
-#Write to CSV
-write.csv(StatsFull, file = "311Stats.CSV")
-
-
-
+#Had an issue where I needed to map the data out by timeline and the funciton needed the last day of the month to properly space things
+#This code generates a dataframe of the last days of the month
+Date.End.Month <- seq(as.Date("2005-02-01"),length=240,by="months")-1
+Date.End.Month <- as.data.frame(Date.End.Month)
+Date.End.Month <- mutate(Date.End.Month, Month.Year =  paste((year(Date.End.Month)), month(Date.End.Month),sep = "-"))
+PipedTestAllData <- mutate(PipedTestAllData , Month.Year =  paste(CREATION.YEAR, CREATION.MONTH, sep = "-"))
+PipedTestAllData <- full_join(PipedTestAllData, Date.End.Month, by = "Month.Year" )
 
