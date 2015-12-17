79b11a2e1905f44aef2db089520b950c3c7ce4df
EROCHE5 <EROCHE5@GMAIL.COM>
2015-12-17 14:23:48 -0600

    Major changes to the scripts to provide better variable names and cut out a bunch of useuless code in the pipes script.

--- Dplyr Simplification.R
+++ Dplyr Simplification.R
@@ -3,13 +3,8 @@ library(RSocrata)
 library(dplyr)
 library(lubridate)
 
-
-token <- XXXXXXX
+#Read in 311 data from Open Data. This uses the RSocrata package.
 Data.311 <- read.socrata("https://data.kcmo.org/311/KCMOPS311-Data/7at3-sxhp")
-
-#Inspect the data
-nrow(Data.311)
-Work.Groups <- names(table(Data.311$WORK.GROUP))
 
 #Clean Data
 Data.311$CREATION.DATE <- as.POSIXct(Data.311$CREATION.DATE)
@@ -23,22 +18,25 @@ Data.311$Number.Of.Open.Cases[Data.311$Number.Of.Open.Cases != "1"] <- 0
 Data.311$Number.Of.Open.Cases <- as.numeric(Data.311$Number.Of.Open.Cases)
 Data.311$DAYS.TO.CLOSE <- as.numeric(Data.311$DAYS.TO.CLOSE)
 Current.Date <- as.POSIXct(Sys.Date())
-Current.Date <- round(Current.Date, "days")
 
 
-#Add in a column for Days to Close for Open and closed cases
+
+
+#Filters 311 Data down to only Open and Closed cases
 Data.311 <- filter(Data.311, STATUS == "OPEN" | STATUS == "RESOL")
 
-
-
+#Add in a column for Days to Close for Open and closed cases
 Data.312 <- Data.311%>%
               filter(STATUS == "OPEN") %>%
                 select(CASE.ID, CREATION.DATE) %>%
-                  mutate(Days.Open.For.Open.Cases.Only = as.numeric(Current.Date - CREATION.DATE)) %>%
+                  mutate(Days.Open.For.Open.Cases.Only = (Current.Date - CREATION.DATE)) %>%
                     select(CASE.ID, Days.Open.For.Open.Cases.Only)
+#Subracting PosixCT results in the number of seconds, this converts it to days.and then rounds off the decimals
+Data.312$Days.Open.For.Open.Cases.Only <- as.numeric(Data.312$Days.Open.For.Open.Cases.Only, units="days")
+Data.312$Days.Open.For.Open.Cases.Only <- round(Data.312$Days.Open.For.Open.Cases.Only, digits = 0)
                       
 Data.311 <- merge(x = Data.311, y = Data.312, by = "CASE.ID", all.x = TRUE)
-#Add two days to close columns together columns together
+#Add two days to close columns together
 #Generates a column with "days open" for closed and open cases"
 Data.311$Days.Open = rowSums(cbind(Data.311$Days.Open.For.Open.Cases.Only, Data.311$DAYS.TO.CLOSE), na.rm=TRUE)
 
--- Pipes Test Script.R
+++ Pipes Test Script.R
@@ -1,58 +1,41 @@
 #P1 includes all data
 
+Stats <- Data.311 %>% 
+  filter(STATUS == "RESOL" | STATUS == "OPEN") %>%
+  group_by(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH) %>%
+  summarise(
+    Total.Number.Of.Cases = length(CASE.ID),
+    Number.Of.Open.Cases = sum(Number.Of.Open.Cases),
+    Number.Of.Cases.Exceeding.Timeframe = sum(EXCEEDED.EST.TIMEFRAME),
+    Percent.Of.Cases.Exceeding.Timeframe = (Number.Of.Cases.Exceeding.Timeframe/Total.Number.Of.Cases)*100,
+    Mean.Days.To.Close.Of.Closed.Cases = mean(DAYS.TO.CLOSE),
+    Median.Days.To.Close.Closed.Cases = (median(DAYS.TO.CLOSE)*1),
+    Standard.Deviation = sd(DAYS.TO.CLOSE, na.rm=TRUE),
+    Above.One.Standard.Deviation = (Standard.Deviation + Mean.Days.To.Close.Of.Closed.Cases),
+    Above.Two.Standard.Deviations = ((Standard.Deviation*2) + Mean.Days.To.Close.Of.Closed.Cases),
+    Above.Three.Standard.Deviations = ((Standard.Deviation*3) + Mean.Days.To.Close.Of.Closed.Cases),
+    Below.One.Standard.Deviation = (Mean.Days.To.Close.Of.Closed.Cases - Standard.Deviation),
+    Below.Two.Standard.Deviations = (Mean.Days.To.Close.Of.Closed.Cases - (Standard.Deviation*2)),
+    Below.Three.Standard.Deviations = (Mean.Days.To.Close.Of.Closed.Cases - (Standard.Deviation*3))) %>%
+  mutate(UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
 
-PipesTestP1 <- Data.311 %>% 
-                filter(STATUS == "RESOL" | STATUS == "OPEN") %>%
-                  group_by(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH) %>%
-                    summarise(
-                      Total.Number.Of.Cases = length(CASE.ID),
-                      Number.Of.Open.Cases = sum(Number.Of.Open.Cases),
-                      Number.Of.Cases.Exceeding.Timeframe = sum(EXCEEDED.EST.TIMEFRAME),
-                      Percent.Of.Cases.Exceeding.Timeframe = (Number.Of.Cases.Exceeding.Timeframe/Total.Number.Of.Cases)*100
-                      )
-                      
-#P2 only calculated on closed data
-PipesTestP2 <- Data.311 %>% 
-                filter(STATUS == "RESOL" | STATUS == "OPEN" & length(CASE.ID) > 1) %>%
-                  group_by(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH) %>%
-                    summarise(
-                      Mean.Days.To.Close.Of.Closed.Cases = mean(DAYS.TO.CLOSE),
-                      Median.Days.To.Close.Closed.Cases = (median(DAYS.TO.CLOSE)*1),
-                      Standard.Deviation = sd(DAYS.TO.CLOSE, na.rm=TRUE),
-                      Above.One.Standard.Deviation = (Standard.Deviation + Mean.Days.To.Close.Of.Closed.Cases),
-                      Above.Two.Standard.Deviations = ((Standard.Deviation*2) + Mean.Days.To.Close.Of.Closed.Cases),
-                      Above.Three.Standard.Deviations = ((Standard.Deviation*3) + Mean.Days.To.Close.Of.Closed.Cases),
-                      Below.One.Standard.Deviation = (Mean.Days.To.Close.Of.Closed.Cases - Standard.Deviation),
-                      Below.Two.Standard.Deviations = (Mean.Days.To.Close.Of.Closed.Cases - (Standard.Deviation*2)),
-                      Below.Three.Standard.Deviations = (Mean.Days.To.Close.Of.Closed.Cases - (Standard.Deviation*3)))
-                
-#Join the tables together
-#Add a unique identified that is shared between both datasets.
-
-PipesTestP1 <- mutate(PipesTestP1, UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
-PipesTestP2 <- mutate(PipesTestP2, UniqueID = paste(DEPARTMENT, WORK.GROUP, CREATION.YEAR, CREATION.MONTH, sep = " "))
-
-#Drops duplicate columns but retains unique ID then combines the two data frames. 
-PipesTestP2 <- subset(PipesTestP2, select = -c(DEPARTMENT:CREATION.MONTH))
-PipedTestAllData <- full_join(PipesTestP1, PipesTestP2, by = "UniqueID")
-
-#Had an issue where I needed to map the data out by timeline and the funciton needed the last day of the month to properly space things
+#Had an issue where I needed to map the data out by timeline and the function needed the last day of the month to properly space things
 #This code generates a dataframe of the last days of the month
 Date.End.Month <- seq(as.Date("2005-02-01"),length=240,by="months")-1
 Date.End.Month <- as.data.frame(Date.End.Month)
 Date.End.Month <- format(Date.End.Month$Date.End.Month, "%Y/%m/%d")
 Date.End.Month <- as.data.frame(Date.End.Month)
 Date.End.Month <- mutate(Date.End.Month, Month.Year =  paste((year(Date.End.Month)), month(Date.End.Month),sep = "."))
-PipedTestAllData <- mutate(PipedTestAllData , Month.Year =  paste(CREATION.YEAR, CREATION.MONTH, sep = "."))
-PipedTestAllData <- full_join(PipedTestAllData, Date.End.Month, by = "Month.Year" )
-PipedTestAllData$Date.End.Month <- as.POSIXct(PipedTestAllData$Date.End.Month)
+Stats <- mutate(Stats , Month.Year =  paste(CREATION.YEAR, CREATION.MONTH, sep = "."))
+Stats <- full_join(Stats, Date.End.Month, by = "Month.Year" )
+Stats$Date.End.Month <- as.POSIXct(Stats$Date.End.Month)
 
 #Add in Fiscal Year
 FiscalYears <- seq(as.POSIXct("2000-05-01"), length=35, by="year")
-PipedTestAllData$Creation.Fiscal.Year <- (2001:2025)[ findInterval(PipedTestAllData$Date.End.Month, FiscalYears)]
+Stats$Creation.Fiscal.Year <- (2001:2025)[ findInterval(Stats$Date.End.Month, FiscalYears)]
 
 #Arrange variables in a better order
-PipedTestAllData <- PipedTestAllData[,c(
+Stats <- Stats[,c(
   "DEPARTMENT",
   "WORK.GROUP",
   "Creation.Fiscal.Year",
@@ -76,7 +59,6 @@ PipedTestAllData <- PipedTestAllData[,c(
   "UniqueID"
 )]
 
-
-write.csv(PipedTestAllData, file = "311Stats.csv")
+write.csv(Stats, file = "311Stats.csv")
 
 
